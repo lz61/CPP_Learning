fc674418d4fd0eab58c0d0c225e8ab7afba3c0b0
lz61 <71430392+lz61@users.noreply.github.com>
2025-04-07 06:49:17 +0800

    Local Recover

--- code/test.cpp
+++ code/test.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(void){
-    int x= 42;
-    cout<<x<<endl;
-    cout<<hex;
-    cout<<x<<endl;
-    cout<<oct;
-    cout<<x<<endl;
+    char x = 'a';
+    cout.put(x);
 }
